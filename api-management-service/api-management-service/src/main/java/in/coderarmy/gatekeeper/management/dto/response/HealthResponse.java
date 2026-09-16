b689fa092bfae09bccdd40b616c6930074ef4c9a
sangeeta2003 <mishrasangeeta944@gmail.com>
2026-09-16 11:51:23 +0530

    create database in api management service

--- api-management-service/api-management-service/src/main/java/in/coderarmy/gatekeeper/management/dto/response/HealthResponse.java
+++ api-management-service/api-management-service/src/main/java/in/coderarmy/gatekeeper/management/dto/response/HealthResponse.java
@@ -4,12 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @AllArgsConstructor
 @Getter
 @Setter
 public class HealthResponse {
-    private String serviceName;
-    private String serviceDescription;
-    private String appStatus;
-
+    private String serviceStatus;
+    private String dbStatus;
+    private LocalDateTime timestamp;
 }
